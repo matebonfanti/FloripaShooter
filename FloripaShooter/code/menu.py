5197cb2710538557e5d7adb4c2ab232b72f18028
matebonfanti <matebonfanti@gmail.com>
2025-08-24 21:22:49 -0300

    Ajustes de arquivos, adicionado UML Files

---
+++ FloripaShooter/code/menu.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Menu:
+    def __init__(self):
+        self.window = None
+        self.name = None
+        self.enemy_list = None
+
+    def run(self, ):
+        pass
